4a7f503775add7f35f23d25a23ff508984dbb231
jaydenosafo <149180207+jaydenosafo@users.noreply.github.com>
2024-05-18 11:31:07 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,21 @@
 function processArray (arr) {
   return arr.map(num => (num % 2 === 0) ? num ** 2 : num * 3);
 }
+
+const numbers = [1, 2, 3, 4];
+const newNumbers = processArray(numbers);
+console.log(newNumbers);
+
+function formatArrayStrings(strArr, numArr) {
+  return strArr.map((str, index) =>
+  (numArr[index] % 2 === 0) ? str.toUpperCase() : str.toLowerCase() );
+}
+
+const strArr = ["Jayden", "Kofi", "Ama", "justin"];
+const numArr = processArray(numbers);
+const finalOutput = formatArrayStrings(strArr, numArr);
+console.log(finalOutput);
+
+module.exports = {
+  processArray, formatArrayStrings, strArr, numbers
+}
